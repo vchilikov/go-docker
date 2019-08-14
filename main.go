0fa6f76c628784d082e0da6e6c69f3f52229108d
vchilikov <vchilikov@gmail.com>
2019-08-14 23:49:41 +0300

    liveness probe

--- main.go
+++ main.go
@@ -19,8 +19,28 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+var cnt int
+
+func health(w http.ResponseWriter, r *http.Request) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	cnt++
+	if cnt > 4 {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	_, err = fmt.Fprintf(w, "Hello! Hostname: %s Cnt: %d", hostname, cnt)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
 func main() {
-	http.HandleFunc("/", index) // установим роутер
+	http.HandleFunc("/", index)
+	http.HandleFunc("/health", health)
 	fmt.Println(" http://localhost:9000")
 	err := http.ListenAndServe(":9000", nil) // задаем слушать порт
 	if err != nil {
